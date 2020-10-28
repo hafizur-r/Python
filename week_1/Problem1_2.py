ae119c79da0980540aaf7d1445757d883dd0c9c1
Md Hafizur Rahman <hafizur.raj@gmail.com>
2020-10-28 01:14:35 -0600

    Add files via upload

---
+++ week_1/Problem1_2.py
@@ -0,0 +1,21 @@
+#%%
+
+"""
+Problem 1_2:
+Write a function problem1_2(x,y) that prints the sum and product of the
+numbers x and y on separate lines, the sum printing first.
+"""
+#%%
+def problem1_2(x,y):
+    su=x+y
+    prod=x*y
+    print(su)
+    print(prod)
+#%% 
+"""
+Test run. Note that the grader program will use different numbers:
+
+problem1_2(3,5)
+8
+15
+""" 
